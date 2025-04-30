719a1805310d49d20567518fea6dfedfef15c43e
renatoads1 <renato.lacerda>
2025-04-30 15:42:18 -0300

    Adicionar arquivos de projeto.

---
+++ Funcionarios/Entities/Funcionarios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionarios.Entities
+{
+    public class Funcionarios
+    {
+        public string Nome { get; set; }
+        public int Hora { get; set; }
+        public double ValorPorHora { get; set; }
+
+        public Funcionarios()
+        {
+        }
+        public Funcionarios(string nome, int hora, double valorPorHora)
+        {
+            Nome = nome;
+            Hora = hora;
+            ValorPorHora = valorPorHora;
+        }
+
+        public virtual double Pagamento()
+        {
+            return Hora * ValorPorHora;
+        }
+    }
+}
---
+++ Funcionarios/Entities/FuncionariosTerceirizados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionarios.Entities
+{
+    public class FuncionariosTerceirizados :Funcionarios
+    {
+        public double MaterialGasto { get; set; }
+
+        public FuncionariosTerceirizados(string nome, int hora, double valorPorHora, double materialGasto)
+            :base(nome, hora, valorPorHora)
+        {
+            Nome = nome;
+            Hora = hora;
+            ValorPorHora = valorPorHora;
+            MaterialGasto = materialGasto;
+        }
+
+        public override double Pagamento()
+        {
+            var pag =  base.Pagamento(); 
+            pag = pag + (MaterialGasto * 0.1);
+            return pag;
+        }
+
+    }
+}
